89bb0b5dcc5ab2d2309a0aa8cd14233bd219941e
princexoleo <xtleon.xl@gmail.com>
2021-06-04 23:57:07 +0600

    Modify the photo upload folers name, and remigrate the databases

--- realtors/models.py
+++ realtors/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 # Create your models here.
 class Realtor(models.Model):
     name  = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to="photo/%Y/%m/%d")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.EmailField()
